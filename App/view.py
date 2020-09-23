c1e4aa781dbf73f5ed7e783319877b9b2ccba030
SAPRIAL <69648390+SAPRIAL@users.noreply.github.com>
2020-09-23 07:10:32 -0500

    Entrega Final – Reto 2

--- App/view.py
+++ App/view.py
@@ -40,24 +40,13 @@ operación seleccionada.
 # ___________________________________________________
 
 moviesfile="AllMoviesDetailsCleaned.csv"
+moviesfile2="AllMoviesCastingRaw.csv"
 
 
 # ___________________________________________________
 #  Funciones para imprimir la inforamación de
 #  respuesta.  La vista solo interactua con
 #  el controlador.
-# ___________________________________________________
-
-def printFirstandLast(catalog,titulo,fecha,promedio,votos,idioma,tamaño,pos):
-    print(controller.FirstandLastElementsNTFPVI(catalog,titulo,fecha,promedio,votos,idioma,tamaño,pos))
-
-
-def infoProductor(catalog, producer):
-    print(controller.infoProductor(catalog, producer))
-
-
-# ___________________________________________________
-#  Menu principal
 # ___________________________________________________
 
 def printFirstandLast(catalog,titulo,fecha,promedio,votos,idioma,tamaño,pos):
@@ -71,8 +60,7 @@ def infoGenero(catalog, genero):
   
 def infoPais(catalog, pais):
     print(controller.infoPais(catalog, pais))
-
-
+  
 # ___________________________________________________
 #  Menu principal
 # ___________________________________________________
@@ -83,8 +71,10 @@ def printMenu():
     print("2 - Cargar informacion en el catalogo")
     print("3 - Imprimir primera y ultima pelicula")
     print("4 - Informacion de una productora")
-    print("5 - Informacion de un Genero")
-    print("6 - Peliculas producidas en un pais")
+    print("5 - Informacion de un director")
+    print("6 - Información de un actor")
+    print("7 - Informacion de un Genero")
+    print("8 - Peliculas producidas en un pais")
     print("0 - Salir")
 
 
@@ -99,8 +89,9 @@ while True:
 
     elif int(inputs[0]) == 2:
         print("Cargando información de los archivos ....")
-        controller.loadData(cont, moviesfile)
-        print('peliculas cargados: ' + str(controller.movieSize(cont)))
+        controller.loadData(cont, moviesfile, moviesfile2)
+        print('Películas cargadas: ' + str(controller.movieSize(cont)))
+        print('Películas cargadas: ' + str(controller.castingSize(cont)))
 
     elif int(inputs[0]) == 3:
         Tamaño = controller.movieSize(cont)
@@ -116,24 +107,24 @@ while True:
         Idioma2 = controller.Idioma(cont, Tamaño)
         printFirstandLast(cont,Titulo1, Fecha1, Promedio1, Votos1, Idioma1,Tamaño,1)
         printFirstandLast(cont,Titulo2, Fecha2, Promedio2, Votos2, Idioma2,Tamaño,Tamaño)
-
-    elif int(inputs[0]) == 4:
-        t1_start = process_time()
-        producer = input("Ingrese el nombre del productor:  ")
-        print(infoProductor(cont, producer))
-        t1_stop = process_time()
-        print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
-
     
     elif int(inputs[0]) == 4:
         producer = input("Ingrese el nombre del productor:  ")
         print(infoProductor(cont, producer))
-       
+    
     elif int(inputs[0]) == 5:
+        director = input("Ingrese el nombre del director:  ")
+        print(controller.infoDirector(cont, director))
+       
+    elif int(inputs[0]) == 6:
+        actor = input("Ingrese el nombre del actor:  ")
+        print(infoActor(cont, actor))
+       
+    elif int(inputs[0]) == 7:
         genero = input("Ingrese el genero:  ")
         print(infoGenero(cont, genero))
        
-    elif int(inputs[0]) == 5:
+    elif int(inputs[0]) == 8:
         pais = input("Ingrese el pais:  ")
         print(infoPais(cont, pais))
            
